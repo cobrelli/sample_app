0918f457212bb341e94729a9c3ce1647ce28629b
Tomi Virtanen <cobrelli@gmail.com>
2013-05-20 16:00:59 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
   #get "static_pages/home"
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
+  
+  match '/signup',   to: 'users#new'
+
+  match '/help',     to: 'static_pages#help'
+  match '/about',    to: 'static_pages#about'
   match '/contacts', to: 'static_pages#contacts'
 
   #get "static_pages/help"
